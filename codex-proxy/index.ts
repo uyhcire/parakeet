e0a12a4bd5ebc6d24cefb00f3b0d200649b74771
Eric Yu <ericyu3@gmail.com>
2022-06-20 23:36:58 +0000

    Implement content filter

--- codex-proxy/index.ts
+++ codex-proxy/index.ts
@@ -26,6 +26,19 @@ app.post(
   "/codex",
   checkJwt,
   async (req: express.Request, res: express.Response) => {
+    if (
+      process.env.OPENAI_API_KEY == null ||
+      process.env.OPENAI_API_KEY.length === 0
+    ) {
+      console.log(
+        "No OpenAI API key was configured. You may need to run `flyctl secrets import`."
+      );
+      res.status(500).send(
+        { data: {} } // avoid giving away any sensitive information
+      );
+      return;
+    }
+
     // We can use `sub` to identify the user, as it is guaranteed to be unique.
     const userSubId = req.auth?.payload?.sub;
     if (!userSubId) {
@@ -42,7 +55,7 @@ app.post(
       return;
     }
 
-    const startTime = new Date();
+    const lmRequestStartTime = new Date();
 
     const lmResponse = await fetch("https://api.openai.com/v1/completions", {
       method: "POST",
@@ -60,10 +73,6 @@ app.post(
         user: userSubId,
       }),
     });
-    const lmResponseJson = (await lmResponse.json()) as any;
-
-    const lmInferenceMillis = new Date().getTime() - startTime.getTime();
-    console.log(`LM inference call took ${lmInferenceMillis}ms`);
 
     if (lmResponse.status !== 200) {
       res
@@ -80,9 +89,69 @@ app.post(
       return;
     }
 
+    const lmResponseJson = (await lmResponse.json()) as any;
+
+    const lmInferenceMillis =
+      new Date().getTime() - lmRequestStartTime.getTime();
+    console.log(`LM inference call took ${lmInferenceMillis}ms`);
+
+    const completion = lmResponseJson.choices![0].text!.split("\n")[0];
+    let cfInferenceMillis: number | null = null;
+    let isToxic: boolean = false;
+    if (completion.length > 0) {
+      const cfRequestStartTime = new Date();
+
+      const cfResponse = await fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          model: "content-filter-alpha",
+          prompt: `<|endoftext|>${completion}\n--\nLabel:`,
+          max_tokens: 1,
+          temperature: 0.0,
+          top_p: 0,
+          logprobs: 10,
+          // Probably not necessary, but it doesn't hurt.
+          user: userSubId,
+        }),
+      });
+
+      if (cfResponse.status !== 200) {
+        res
+          .status(
+            cfResponse.status === 429
+              ? 429
+              : // No other kind of error is expected
+                500
+          )
+          .header("Content-Type", "application/json")
+          .send(
+            { data: {} } // avoid giving away any sensitive information
+          );
+        return;
+      }
+
+      const cfResponseJson = (await cfResponse.json()) as any;
+
+      cfInferenceMillis = new Date().getTime() - cfRequestStartTime.getTime();
+      console.log(`Content filter inference call took ${cfInferenceMillis}ms`);
+
+      const outputLabel = cfResponseJson.choices![0].text;
+      const logprobs = cfResponseJson.choices![0].logprobs!.top_logprobs![0];
+      const toxicThreshold = -0.355; // as recommended by OpenAI
+
+      if (outputLabel === "2" && logprobs["2"] >= toxicThreshold) {
+        isToxic = true;
+      }
+    }
+
     res.header("Content-Type", "application/json").send({
-      completion: lmResponseJson.choices![0].text!.split("\n")[0],
+      completion: !isToxic ? completion : "",
       lmInferenceMillis,
+      cfInferenceMillis,
     });
   }
 );

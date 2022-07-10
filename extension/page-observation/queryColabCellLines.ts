aa2653fdc9f4296c9c7b01a399deff1acccb75cc
Eric Yu <ericyu3@gmail.com>
2022-07-10 02:15:57 +0000

    Fix lint

--- extension/page-observation/queryColabCellLines.ts
+++ extension/page-observation/queryColabCellLines.ts
@@ -1,5 +1,5 @@
 const parseOffsetTop = (topAttr: string): number => {
-  const parseResult = /^([0-9\.]+)px$/.exec(topAttr);
+  const parseResult = /^([0-9.]+)px$/.exec(topAttr);
   if (parseResult == null) {
     throw new Error(`Unexpected format of CSS \`top\` attribute: "${topAttr}"`);
   }
